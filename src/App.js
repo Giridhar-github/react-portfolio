bc7df0ebe1cedabe82df3856e123587bce4b66c3
Giridhar-github <57524550+Giridhar-github@users.noreply.github.com>
2023-07-24 00:19:01 +0530

    added authentication halfway

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import './App.css';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Main from './components/Main';
-import SubMain from './components/SubMain';
-import Home from './pages/Home';
-import Login from './pages/Login';
+import { useState } from "react";
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import SubMain from "./components/SubMain";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
+  const [isVerified, setVerified] = useState(false);
+  const onVerifying = (data) => {
+    console.log(data);
+  };
   return (
     <div>
-    {/* <Home /> */}
-    <Login />
+      {!isVerified && <Login onSubmitData={onVerifying} />}
+      {isVerified && <Home />}
     </div>
   );
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,33 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { FaFacebook, FaGoogle, FaTwitter, FaTwitterSquare } from "react-icons/fa";
+import {
+  FaFacebook,
+  FaGoogle,
+  FaTwitter,
+  FaTwitterSquare,
+} from "react-icons/fa";
 
-const Login = () => {
+const Login = (props) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const[valid, setValid]=useState(false);
+  const[errorMessage, setErrorMessage]=useState(false);
+  let userEmail="giridharm555@gmail.com";
+  let userPassword="123456789";
+  const inputEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const inputPassword = (e) => {
+    setPassword(e.target.value);
+  };
+const formValidate=(e)=>{
+  e.preventDefault();
+  if(email===userEmail && password===userPassword){
+    setValid(true);
+    setErrorMessage(false);
+  }else{
+    setValid(false);
+    setErrorMessage(true);
+  }
+
+}
   return (
     <div className="login-page">
-      <div className="login-container">
+      <form className="login-container" onSubmit={formValidate}>
         <div className="login-logo">Falmengo</div>
         <hr />
         <div className="login-input">
           <div className="input-credentials">
             <label htmlFor="Email">Email Id:</label>
-            <input type="text" placeholder="Enter your Email Id" />
+            <input
+              type="text"
+              placeholder="Enter your Email Id"
+              onChange={inputEmail}
+            />
           </div>
           <div className="input-credentials">
             <label htmlFor="Password">Password:</label>
-            <input type="password" placeholder="Enter your password" />
+            <input
+              type="password"
+              placeholder="Enter your password"
+              onChange={inputPassword}
+            />
           </div>
           <div className="input-button">
-            <button>LOGIN</button>
+            <button type="submit">LOGIN</button>
           </div>
+          {errorMessage && <div className="errorBox"><center><p><i>*Invalid Credentials*</i></p></center></div>}
         </div>
         <div className="login-options">
-            <a href=""><FaGoogle size={20} /></a>
-            <a href=""><FaTwitter size={20} /></a>
-            <a href=""><FaFacebook size={20} /></a>
+          <a href="">
+            <FaGoogle size={20} />
+          </a>
+          <a href="">
+            <FaTwitter size={20} />
+          </a>
+          <a href="">
+            <FaFacebook size={20} />
+          </a>
         </div>
-      </div>
+      </form>
     </div>
   );
 };
